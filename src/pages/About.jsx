8d3fe593b979cfd06a010c8edb94b819958121c2
Aman Kumar Agrawal <582002aman@gmail.com>
2022-09-12 08:04:32 +0530

    change in about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,14 +6,14 @@ function About() {
       <h1 className="text-6xl mb-4">Github Finder</h1>
       <p className="mb-4 text-2xl font-light">
         A React app to search GitHub profiles and see profile details.
-        
       </p>
+    
       <p className="text-lg text-gray-400">
-        Version <span className="text-white">1.0.0</span>
+        Version <span className="text-primary">1.0.0</span>
       </p>
       <p className="text-lg text-gray-400">
         Developed By:
-        <a className="text-white" href="https://twitter.com/amagrawal_20">
+        <a className="text-primary hover:text-black" href="https://twitter.com/amagrawal_20" target="_blank" rel="noreferrer">
           {" Aman Agrawal"}
         </a>
       </p>
